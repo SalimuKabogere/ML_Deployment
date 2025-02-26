3bab8bef20f7fee4138598fc17e9902f1eecd4a4
Kabogere Salimu <salimuloma@gmail.com>
2025-02-26 20:27:00 +0600

    Update settings.py
    updated settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qv$uppuyu+f&@bf=vxkte51t9q3*+gvjp*ym@ic7m#w6h-&amn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['fakemajja.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
